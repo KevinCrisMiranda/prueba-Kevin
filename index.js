5474c4411eecde98be1dd5f74c5f212b2f71f80a
KevinMiranda75 <kevin.miranda@lexis.com.ec>
2024-05-04 16:39:10 -0500

    agregando cambios num ale

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 require("dotenv").config()
 app.set('port', process.env.PORT || process.env.PORTSEC);
 app.get('/', (req,res) =>  res.send('Hola Kevin'));
+app.get('/load', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    res.send(randomNumber.toString());
+  });
+
 app.listen(app.get('port'), function() {
     console.log('Server on port-'+ process.env.PORT+'-' + process.env.PORTSEC);
   });
